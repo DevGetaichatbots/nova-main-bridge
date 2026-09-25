f12551eac9bbf79e48c84c09954a764536bb93cb
Meistaaa <alimuhammad9868@gmail.com>
2026-09-25 16:57:28 +0500

    feat: add additional allowed origin for CORS

--- Nova-Insights-Backend/app.py
+++ Nova-Insights-Backend/app.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 
 ALLOWED_ORIGINS = [
     "https://green-beach-00b29e810.7.azurestaticapps.net",
+    "https://mango-beach-0741f7103.6.azurestaticapps.net",
     "https://nova-insights-frontend.azurewebsites.net",
     "https://dashboard.novainsight.net",
     "https://www.novainsight.net",
